9e891f75db69e8f2ec1d11b7c94465640b254e6d
martingerbaldo <gerbaldomartin@gmail.com>
2021-04-18 18:52:30 -0300

    agregado de la funcion pow, por Martin

--- fns.js
+++ fns.js
@@ -31,6 +31,8 @@ function div(a, b) {
     // retorna a/b, salvo que b sea 0. Agreado por Augusto.
 }
 
-function pow(a, b) {}
-
+function pow(a, b) {
+    return a ^ b;
+    //retorna a-b, en ese orden. Agregado por Martin Gerbaldo
+}
 module.exports = fns;
